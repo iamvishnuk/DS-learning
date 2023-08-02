70c15849db564395c8601ee115bfdf048780f33e
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2023-08-02 10:24:49 +0530

    add leetcode solution for 557, 657 and 1967

---
+++ LeetCode/557.js
@@ -0,0 +1,13 @@
+// 557. Reverse words in a string III
+
+var reverseWords = function (s) {
+    const wordsArray = s.split(' ');
+    const reversedWordsArray = wordsArray.map(word => {
+        return word.split("").reverse().join("")
+    })
+    return reversedWordsArray.join(" ")
+};
+
+const s = "Let's take LeetCode contest"
+
+console.log(reverseWords(s))

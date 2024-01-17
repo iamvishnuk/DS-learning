2276ed6a9bf8f7c74f4ccb1fdfdb254c8d89159a
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2024-01-17 20:21:45 +0530

    add leetcode soultion for 143 and 207

---
+++ LeetCode/Medium/207.js
@@ -0,0 +1,50 @@
+// 207. Course Schedule
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function (numCourses, prerequisites) {
+    let adjList = {};
+    let visited = new Set();
+
+    for (let [a, b] of prerequisites) {
+        if (!adjList[a]) {
+            adjList[a] = [b];
+        } else {
+            adjList[a].push(b);
+        }
+    }
+
+    function dfs(curr) {
+        if (visited.has(curr)) return false;
+        if (adjList[curr] === []) return true;
+
+        visited.add(curr);
+        if (adjList[curr]) {
+            for (let neigh of adjList[curr]) {
+                if (!dfs(neigh)) {
+                    return false;
+                }
+            }
+        }
+
+        visited.delete(curr);
+        adjList[curr] = [];
+
+        return true;
+    }
+
+    for (let key in adjList) {
+        if (!dfs(key)) {
+            return false;
+        }
+    }
+    return true;
+};
+
+const numCourses = 2;
+const prerequisites = [
+    [1, 0],
+    [0, 1],
+];

8d4290b68c4b69b46e1fa25291f746bcf7877ea1
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2025-03-01 15:43:38 +0530

    add leetcode solution for 2460

---
+++ LeetCode/Easy/2460.js
@@ -0,0 +1,26 @@
+// 2460. Apply Operations to an Array
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var applyOperations = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums[i] = nums[i] * 2;
+      nums[i + 1] = 0;
+    }
+  }
+
+  // shift all zeros to the end while maintaining order
+  let nonZeroIndex = 0;
+  // Move all non-zero elements to the front
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      [nums[nonZeroIndex], nums[i]] = [nums[i], nums[nonZeroIndex]];
+      nonZeroIndex++;
+    }
+  }
+
+  return nums;
+};

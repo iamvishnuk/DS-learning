d2a873098a3f6e7b64482e53586375ed79792904
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2025-12-01 22:45:39 +0530

    feat: MazeSolver

---
+++ typescript/problems/MazeSolver.ts
@@ -0,0 +1,77 @@
+const dir = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1]
+];
+
+export type Point = {
+  x: number;
+  y: number;
+};
+
+const walk = (
+  maze: string[],
+  wall: string,
+  curr: Point,
+  end: Point,
+  seen: boolean[][],
+  path: Point[]
+): boolean => {
+  // Base Case
+  // 1. Off the map
+  if (
+    curr.x < 0 ||
+    curr.x >= maze[0].length ||
+    curr.y < 0 ||
+    curr.y >= maze.length
+  ) {
+    return false;
+  }
+
+  // On the wall
+  if (maze[curr.y][curr.x] === wall) {
+    return false;
+  }
+
+  if (curr.x === end.x && curr.y === end.y) {
+    path.push(curr);
+    return true;
+  }
+
+  if (seen[curr.y][curr.x]) {
+    return false;
+  }
+
+  seen[curr.y][curr.x] = true;
+  path.push(curr);
+
+  for (let i = 0; i < dir.length; i++) {
+    const [x, y] = dir[i];
+    if (walk(maze, wall, { x: curr.x + x, y: curr.y + y }, end, seen, path)) {
+      return true;
+    }
+  }
+
+  path.pop();
+
+  return false;
+};
+
+export const MazeSolver = (
+  maze: string[],
+  wall: string,
+  start: Point,
+  end: Point
+) => {
+  const seen: boolean[][] = [];
+  const path: Point[] = [];
+
+  for (let i = 0; i < maze.length; i++) {
+    seen.push(new Array(maze[0].length).fill(false));
+  }
+
+  walk(maze, wall, start, end, seen, path);
+
+  return path;
+};

a0c18073b20301ceb183b68b1a1d2f0467044bc5
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2025-12-04 22:54:24 +0530

    feat: Leetcode solution 700

---
+++ LeetCode/Easy/700.ts
@@ -0,0 +1,26 @@
+// 700. Search in a Binary Search Tree
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+  if (!root) return null;
+
+  // check if the root.value === value
+  if (root.val == val) return root;
+
+  if (root.val < val) {
+    return searchBST(root.right, val);
+  }
+
+  return searchBST(root.left, val);
+}

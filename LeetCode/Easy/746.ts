99326cb6b7db5657993d8a62542badb78f28ba0c
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2026-01-28 12:08:50 +0530

    feat: leetcode 746

---
+++ LeetCode/Easy/746.ts
@@ -0,0 +1,19 @@
+// 746. Min Cost Climbing Stairs
+
+function minCostClimbingStairs(cost: number[]): number {
+  const n = cost.length;
+
+  const dp: number[] = new Array(n);
+
+  dp[0] = cost[0];
+  dp[1] = cost[1];
+
+  for (let i = 2; i < n; i++) {
+    dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+  }
+
+  return Math.min(dp[n - 1], dp[n - 2]);
+}
+
+console.log(minCostClimbingStairs([10, 15, 20]));
+console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1]));

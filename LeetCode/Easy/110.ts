6fe8b88e95803787241990df19ecd0875305d841
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2026-02-08 10:12:57 +0530

    feat: leetcode 110

---
+++ LeetCode/Easy/110.ts
@@ -0,0 +1,21 @@
+// 110. Balanced Binary Tree
+
+import { TreeNode } from '../../types';
+
+function isBalanced(root: TreeNode | null): boolean {
+  function height(node: TreeNode | null): number {
+    if (!node) return 0;
+
+    const left = height(node.left);
+    if (left === -1) return -1;
+
+    const right = height(node.right);
+    if (right === -1) return -1;
+
+    if (Math.abs(left - right) > 1) return -1;
+
+    return 1 + Math.max(left, right);
+  }
+
+  return height(root) !== -1;
+}

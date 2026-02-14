b5bea99a246a9bf1ea3f7a07b0cb4e93174bc406
VISHNU K <86109601+iamvishnuk@users.noreply.github.com>
2026-02-14 10:29:47 +0530

    feat: leetcode 525

---
+++ LeetCode/Medium/525.ts
@@ -0,0 +1,19 @@
+// 525. Contignous Array
+
+function findMaxLength(nums: number[]): number {
+  const map = new Map<number, number>()
+  map.set(0, -1)
+  let maxLength = 0, count = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    count = count + (nums[i] === 0 ? -1 : 1)
+    if (map.has(count)) {
+      maxLength = Math.max(maxLength, i - map.get(count)!)
+    } else {
+      map.set(count, i)
+    }
+  }
+
+  return maxLength
+
+};
